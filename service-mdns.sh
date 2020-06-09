993f35a0fa63ff7c14c1ffcc08615db83585ae38
John Trimble <trimblej@gmail.com>
2020-06-09 00:33:09 -0700

    Fix execution of update_mdns.py

--- service-mdns.sh
+++ service-mdns.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -e -o pipefail
-kubectl get service --all-namespaces --watch --output=json --output-watch-events=true | update_mdns.py
+kubectl get service --all-namespaces --watch --output=json --output-watch-events=true | python3 update_mdns.py
