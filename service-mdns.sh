c94fffc446c52e6e809820f3cb1b90e9d701fcd7
John Trimble <trimblej@gmail.com>
2020-06-20 15:12:57 -0700

    Workaround for random kubectl watch termination

--- service-mdns.sh
+++ service-mdns.sh
@@ -4,4 +4,9 @@ set -eo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-kubectl get service --all-namespaces --watch --output=json --output-watch-events=true | python3 "$SCRIPT_DIR"/update_mdns.py
+# kubectl will stop watching and exit with exit code 0 on occasion. When this
+# happens just restart it. (https://github.com/kubernetes/kubernetes/issues/42552)
+while true; do
+    kubectl get service --all-namespaces --watch --output=json --output-watch-events=true \
+    && sleep 5;
+done | python3 "$SCRIPT_DIR"/update_mdns.py
